979fcf599094db8d7b9936d9ffa6998fe704cfc7
Gordon Chan <me@gordon-chan.net>
2017-02-02 01:30:40 +0800

    Add HTML/JS code for client side signal triggerring

---
+++ public/js/eventFeed.js
@@ -0,0 +1,36 @@
+var EventFeed = function(socket) {
+  this.socket = socket;
+};
+
+EventFeed.prototype.sendSignal = function( action, productId ) {
+  var data = {
+  	domain: document.domain,
+    productId: productId,
+    action: action
+  };
+  this.socket.emit('signal', data);
+};
+
+
+// ===============================
+
+$(document).ready( function() {
+  var socket = io.connect('http://localhost:8000/eventFeed');
+  var eventFeedApp = new EventFeed(socket);
+
+  socket.on('update', function( data ) {
+    console.log('Received signal');
+    console.log( data );
+  });
+
+  $('#btn').on("click", function() {
+
+  	var productId = $("#txt").val();
+  	var action = $("#sel").val();
+
+  	if( action !== "" ) {
+  		eventFeedApp.sendSignal( action, productId );
+  	}
+
+  });
+});
