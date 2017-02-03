ca3b241ce36072cd4aa446eecb0d87f4083e005e
Gordon Chan <me@gordon-chan.net>
2017-02-03 08:00:22 +0800

    Code refactor to break long js to separate small js

--- server.js
+++ server.js
@@ -1,41 +1,10 @@
 var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var mime = require('mime');
+
 var cache = {};
 var port = process.env.PORT || 8000;
+var serveStatic = require('./lib/serveStaticFnc');
 
-function send404(res) {
-	res.writeHead(404, {'Content-Type': 'text/plain'});
-	res.write('Error 404: resource not found.');
-	res.end();
-}
-
-function sendFile(res, filePath, fileContents) {
-	res.writeHead(200, {'Content-Type': mime.lookup(path.basename(filePath))});
-	res.end(fileContents);
-}
-
-function serveStatic(res, cache, absPath) {
-	if (cache[absPath] && false) {
-		sendFile(res, absPath, cache[absPath]);
-	} else {
-		fs.exists(absPath, function(exists) {
-			if(exists) {
-				fs.readFile(absPath, function(err, data) {
-					if (err) {
-						send404(res);
-					} else {
-						cache[absPath] = data;
-						sendFile(res, absPath, data);
-					}
-				});
-			} else {
-				send404(res);
-			}
-		});
-	}
-}
+require('./lib/serveStaticFnc');
 
 var server = http.createServer(function(req, res) {
 	var filePath = false;
@@ -45,7 +14,7 @@ var server = http.createServer(function(req, res) {
 		filePath = 'public' + req.url;
 	}
 	var absPath = './' + filePath;
-	serveStatic(res, cache, absPath);
+	serveStatic.serveStatic(res, cache, absPath);
 });
 
 
