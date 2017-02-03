45ab902157f67246416b767f360e6629b1ae2120
Gordon CHan <gordonchan@lanecrawford.com>
2017-02-03 09:43:04 +0800

    Fix the string replace code to use regular expression

--- public/js/eventFeed.js
+++ public/js/eventFeed.js
@@ -15,7 +15,7 @@ EventFeed.prototype.sendSignal = function( action, productId ) {
 // ===============================
 
 $(document).ready( function() {
-  var socket = io.connect("[env:endPoint]");
+  var socket = io.connect("%endpoint%");
   var eventFeedApp = new EventFeed(socket);
 
   socket.on('update', function( data ) {
